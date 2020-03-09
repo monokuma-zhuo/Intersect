33a4c80455fed231ec19d9038dea5bbe54cf95da
monokuma-zhuo <875145957@qq.com>
2020-03-09 16:38:06 +0800

    first

---
+++ src/intersect.cpp
@@ -0,0 +1,182 @@
+﻿#include <iostream>
+#include <vector>
+#include <utility>
+#include <set>
+using namespace std;
+class Line
+{
+public:
+	double A, B, C;
+	Line(double x1,double y1,double x2,double y2)
+	{
+		A = y2 - y1;
+		B = x1 - x2;
+		C = x2 * y1 - x1 * y2;
+	}
+	Line(double a, double b, double c) 
+	{
+		A = a;
+		B = b;
+		C = c;
+	}
+	pair<double,double> intersect(Line l)
+	{
+		double x = (l.C * B - C * l.B) / (A * l.B - B * l.A);
+		double y = (C * l.A - l.C * A) / (A * l.B - B * l.A);
+		pair<double, double> dot(x, y);
+		return dot;
+	}
+	bool isteresect(Line l) {
+		if (A * l.B - B * l.A == 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+};
+
+class Cycle
+{
+public:
+	double x, y, r;
+	Cycle(double x, double y, double r)
+	{
+		this->x = x;
+		this->y = y;
+		this->r = r;
+	}
+	bool line_cycle_pos(Line l)
+	{
+		double length = abs((l.A * x + l.B * y + l.C)) / (sqrt(l.A * l.A + l.B * l.B));
+		if (length > r) 
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	pair<pair<double, double>, pair<double, double>> line_cycle_instere(Line l)
+	{
+		if (l.A == 0) 
+		{
+			double y_point = -1.0 * l.C / l.B;
+			double temp = sqrt((r * r - (y - y_point) * (y - y_point)));
+			pair<double, double> dot1(x + temp, y_point);
+			pair<double, double> dot2(x - temp, y_point);
+			return make_pair(dot1, dot2);
+		}
+		else if (l.B == 0)
+		{
+			double x_point = -1.0 * l.C / l.A;
+			double temp = sqrt((r * r - (x - x_point) * (x - x_point)));
+			pair<double, double> dot1(x_point, y + temp);
+			pair<double, double> dot2(x_point, y - temp);
+			return make_pair(dot1, dot2);
+		}
+		else
+		{
+			double k = -1.0 * l.A / l.B;
+			double b = -1.0 * l.C / l.B;	
+			double a1 = 1.0 + k * k;
+			double b1 = 2.0 * (k * b - k * y - x);
+			double c1 = (b - y) * (b - y) -(r * r)+(x*x);
+			double x1 = (-1.0 * b1 + sqrt(b1 * b1 - 4 * a1 * c1)) / (2.0 * a1);
+			double y1 = k * x1 + b;
+			double x2 = (-1.0 * b1 - sqrt(b1 * b1 - 4 * a1 * c1)) / (2.0 * a1);
+			double y2 = k * x2 + b;
+			pair<double, double> dot1(x1, y1);
+			pair<double, double> dot2(x2, y2);
+			return make_pair(dot1, dot2);
+		}
+	}
+	bool cycle_cycel_pos(Cycle c)
+	{
+		double length = sqrt((c.x - x) * (c.x - x) + (c.y - y) * (c.y - y));
+		if (length > r + c.r||length<abs(r-c.r))
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	pair<pair<double, double>, pair<double, double>> cycle_cycle_instere(Cycle c)
+	{
+		double D = -2.0 * x;
+		double E = -2.0 * y;
+		double F = (x * x) + (y * y) - (r * r);
+		double D1 = -2.0 * c.x;
+		double E1 = -2.0 * c.y;
+		double F1 = (c.x * c.x) + (c.y * c.y) - (c.r * c.r);
+		Line temp_l(D - D1, E - E1, F - F1);
+		return line_cycle_instere(temp_l);
+	}
+};
+int main()
+{
+	char type;
+	double x1, x2, y1, y2;
+	double a, b, r;
+	int n;
+	cin >> n;
+	vector<Line> Array_line;
+	vector<Cycle> Array_cycle;
+	set<pair<double, double>> Array_dot;
+	for (int i = 0; i < n; i++) {
+		cin >> type;
+		if (type == 'L') {
+			cin >> x1 >> y1 >> x2 >> y2;
+			Line l = Line(x1, y1, x2, y2);
+			Array_line.push_back(l);
+			for (unsigned int j = 0; j < Array_line.size()-1; j++)
+			{
+				if (Array_line.at(j).isteresect(l))
+				{
+					Array_dot.insert(Array_line.at(j).intersect(l));
+				}
+			}
+			for (unsigned int j = 0; j < Array_cycle.size(); j++) {
+				if (Array_cycle.at(j).line_cycle_pos(l))
+				{
+					pair<pair<double, double>, pair<double, double>> dot = Array_cycle.at(j).line_cycle_instere(l);
+					Array_dot.insert(dot.first);
+					Array_dot.insert(dot.second);
+				}
+			}
+		}
+		if (type == 'C')
+		{
+			cin >> a >> b >> r;
+			Cycle c = Cycle(a, b, r);
+			Array_cycle.push_back(c);
+			for (unsigned int j = 0; j < Array_line.size(); j++)
+			{
+				if (c.line_cycle_pos(Array_line.at(j)))
+				{
+					pair<pair<double, double>, pair<double, double>> dot = c.line_cycle_instere(Array_line.at(j));
+					Array_dot.insert(dot.first);
+					Array_dot.insert(dot.second);
+				}
+			}
+			for (unsigned int j = 0; j < Array_cycle.size() - 1; j++)
+			{
+				if (c.cycle_cycel_pos(Array_cycle.at(j)))
+				{
+					pair<pair<double, double>, pair<double, double>> dot = c.cycle_cycle_instere(Array_cycle.at(j));
+					Array_dot.insert(dot.first);
+					Array_dot.insert(dot.second);
+				}
+			}
+		}
+	}
+	cout << Array_dot.size() << endl;
+	
+}
+
+
---
+++ test/InteresectTestUnit.cpp
@@ -0,0 +1,102 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../intersect/intersect.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace InteresectTestUnit
+{
+	TEST_CLASS(InteresectTestUnit)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Line l1(1, 0, 0, 1);
+			Line l2(0, 0, 2, 2);
+			pair<double, double> dot = l1.intersect(l2);
+			Assert::AreEqual(dot.first, 0.5);
+			Assert::AreEqual(dot.second, 0.5);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			Line l1(1, 0, 0, 1);
+			Line l2(2, 0, 0, 2);
+			bool interesect = l1.isteresect(l2);
+			Assert::AreEqual(interesect, false);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Cycle c1(0, 0, 1);
+			Cycle c2(3, 0, 1);
+			bool flag = c1.cycle_cycel_pos(c2);
+			Assert::AreEqual(flag, false);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			Cycle c1(0, 0, 1);
+			Cycle c2(2, 0, 1);
+			pair<pair<double, double>, pair<double, double>> dot = c1.cycle_cycle_instere(c2);
+			Assert::AreEqual(dot.first.first,1.0);
+			Assert::AreEqual(dot.first.second, 0.0);
+			Assert::AreEqual(dot.second.first, 1.0);
+			Assert::AreEqual(dot.second.second, 0.0);
+		}
+		TEST_METHOD(TestMethod5)
+		{
+			Cycle c1(0, 0, 1);
+			Cycle c2(1, 1, 1);
+			pair<pair<double, double>, pair<double, double>> dot = c1.cycle_cycle_instere(c2);
+			Assert::AreEqual(dot.first.first, 1.0);
+			Assert::AreEqual(dot.first.second, 0.0);
+			Assert::AreEqual(dot.second.first, 0.0);
+			Assert::AreEqual(dot.second.second, 1.0);
+		}
+		TEST_METHOD(TestMethod6)
+		{
+			Cycle c1(0, 0, 2);
+			Cycle c2(1, 0, 1);
+			pair<pair<double, double>, pair<double, double>> dot = c1.cycle_cycle_instere(c2);
+			Assert::AreEqual(dot.first.first, 2.0);
+			Assert::AreEqual(dot.first.second, 0.0);
+			Assert::AreEqual(dot.second.first, 2.0);
+			Assert::AreEqual(dot.second.second, 0.0);
+		}
+		TEST_METHOD(TestMethod7)
+		{
+			Cycle c1(0, 0, 2);
+			Cycle c2(0, 0, 1);
+			bool result = c1.cycle_cycel_pos(c2);
+			Assert::AreEqual(result, false);
+		}
+		TEST_METHOD(TestMethod8)
+		{
+			Cycle c(0, 0, 1);
+			Line l(2, 0, 2, 1);
+			bool result = c.line_cycle_pos(l);
+			Assert::AreEqual(result, false);
+		}
+		TEST_METHOD(TestMethod9)
+		{
+			Cycle c(0, 0, 1);
+			Line l(0, 1, 1, 1);
+			pair<pair<double, double>, pair<double, double>> dot = c.line_cycle_instere(l);
+			Assert::AreEqual(dot.first.first, 0.0);
+			Assert::AreEqual(dot.first.second, 1.0);
+			Assert::AreEqual(dot.second.first, 0.0);
+			Assert::AreEqual(dot.second.second, 1.0);
+			
+		}
+		TEST_METHOD(TestMethod10)
+		{
+			Cycle c(0, 0, 1);
+			Line l(1, 0, 0, 1);
+			pair<pair<double, double>, pair<double, double>> dot = c.line_cycle_instere(l);
+			Assert::AreEqual(dot.first.first, 1.0);
+			Assert::AreEqual(dot.first.second, 0.0);
+			Assert::AreEqual(dot.second.first, 0.0);
+			Assert::AreEqual(dot.second.second, 1.0);
+
+		}
+	};
+}
